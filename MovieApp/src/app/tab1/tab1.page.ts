f2afe1a4c84a5be3644b0f66cdc41bbd76268e3e
Mustafa <mustafa.coskun06@outlook.com>
2020-07-02 11:38:15 +0300

    açıklamalar

--- MovieApp/src/app/tab1/tab1.page.ts
+++ MovieApp/src/app/tab1/tab1.page.ts
@@ -1,39 +1,33 @@
-import { RecentMoviesService } from '../services/recent-movies.service';
-import { from } from 'rxjs';
-import { Component } from '@angular/core';
+import { RecentMoviesService } from "../services/recent-movies.service";
+import { from } from "rxjs";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
-  moviesData: any;
+  moviesData: any; //boş array tanımladık
   slideOpts = {
-    initialSlide: 0,
-    speed: 400,
+    initialSlide: 0, //0. itemdan itibaren
+    speed: 400, //animation hızı
     slidesPerView: 1.4, //item sayısı
   };
-  constructor(public recentService:RecentMoviesService) {
-    this.moviesData=[];
-  
+  constructor(public recentService: RecentMoviesService) {
+    this.moviesData = []; //constructor
   }
   ngOnInit() {
     //lifecyle ilk çalışan
     this.getRecentMovies();
-
   }
-  async getRecentMovies(){
-    this.recentService.getMovies().subscribe(response=>{
-      this.moviesData=response.results;
-      for(var i=0;i<this.moviesData.length;i++){
-        this.moviesData[i].poster_path="http://image.tmdb.org/t/p/w185/"+this.moviesData[i].poster_path;
+  async getRecentMovies() { //async olması lazım yoksa patlayabilir
+    this.recentService.getMovies().subscribe((response) => { //rxjs ile (recent movie servicenin getMovies())
+      this.moviesData = response.results; //data geldiyse reponseyi results a at
+      for (var i = 0; i < this.moviesData.length; i++) { //fotoğraflar path olarak verilmiş linke ekliyoruz
+        this.moviesData[i].poster_path =
+          "http://image.tmdb.org/t/p/w185/" + this.moviesData[i].poster_path;
       }
-      
-    })
+    });
   }
- 
-  
-
 }
