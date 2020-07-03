563c3dbd92025d7be569a5f24d3c6edd8d6246f0
Mustafa <mustafa.coskun06@outlook.com>
2020-07-03 20:51:45 +0300

    basic css

---
+++ cssDeneme/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import{StatusBar} from '@ionic-native/status-bar/ngx'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private statusBar: StatusBar) {
+    this.statusBar.overlaysWebView(false);
+    this.statusBar.styleDefault();
+
+  }
+
+}
